74273400708e43e8568555861d317acad8b04ace
Diego Paula <dih.ferratone@gmail.com>
2020-05-27 16:44:50 -0400

    added function clear and numberDraw

--- app/src/main/java/com/ferratone/sorteioprojeto/MainActivity.java
+++ app/src/main/java/com/ferratone/sorteioprojeto/MainActivity.java
@@ -3,12 +3,36 @@ package com.ferratone.sorteioprojeto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void numeroSorteado(View view){
+
+        TextView numero = findViewById(R.id.txtValorId);
+        int gerarNumero = new Random().nextInt(11);
+        numero.setText(""+gerarNumero);
+    }
+
+    public void clear(View view){
+
+        TextView clearNumber = findViewById(R.id.txtValorId);
+        clearNumber.setText("");
+        Toast.makeText(this,"Valor apagado!",Toast.LENGTH_SHORT).show();
+
     }
 }
